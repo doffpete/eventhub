14fbea15af5a8fed4720ff4059af62f674f3b573
Peter Doff <peter.doff@vendease.com>
2025-08-31 12:03:59 +0100

    updated newlogic for cloudinary storage

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { connectStorageEmulator, getStorage } from "firebase/storage";
 
 const provider = new GoogleAuthProvider();
 
@@ -10,7 +8,6 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
@@ -20,11 +17,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-const storage = getStorage(app);
 
-// Connect to Storage Emulator only in development mode
-if (import.meta.env.MODE === "development") {
-  connectStorageEmulator(storage, "localhost", 9199);
-}
-
-export { auth, provider, signInWithPopup, db, storage };
+export { auth, provider, signInWithPopup, db };
