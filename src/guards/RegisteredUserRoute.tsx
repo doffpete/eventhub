c706fa3d660c4a70f2dd469b78a5b340309ef8e0
Peter Doff <peter.doff@vendease.com>
2025-08-31 03:09:57 +0100

    pushed fix for slight issue in state logic

--- src/guards/RegisteredUserRoute.tsx
+++ src/guards/RegisteredUserRoute.tsx
@@ -1,10 +1,15 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../auth/useAuth";
 import { useLocation } from "react-router-dom";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const RegisteredUserRoute = ({ children }: { children: React.ReactNode }) => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const location = useLocation();
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
 
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
